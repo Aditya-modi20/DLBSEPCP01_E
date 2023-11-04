cc2c02b1669779b2e766f1f35acd2d7f598d28ba
Aditya <77927263+Aditya-modi20@users.noreply.github.com>
2023-11-04 13:38:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,24 +7,7 @@ terraform {
   }
 
   }
-provider "aws" {
-  region = "us-east-2"
-   access_key = ["your access key"]
-  secret_key = ["your security keys"]
 
-  }
-
-/*resource "aws_instance" "Myinstance" {
-  ami = "ami-08333bccc35d71140"
-  instance_type = "t2.micro"
-  key_name = "keypair"
-  vpc_security_group_ids = ["sg-027275b707a68a01e"]
-  user_data = file("data.sh")
-
-  tags = {
-    Name = "Myinstance"
-  }
-} */
 
 resource "aws_default_vpc" "default_vpc" {
   tags = {
